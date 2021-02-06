d24ad58236cdd00f1761b62634997d181bea1f49
Mikhail Lukashenko <k6060037@yandex.ru>
2021-02-06 09:09:07 +0300

    Final

--- src/main/java/yeah/AverageScoreChecker.java
+++ src/main/java/yeah/AverageScoreChecker.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class averageScoreChecker {
+public class AverageScoreChecker {
 
     public static void main(String[] args) {
         List<Student> studentsList = studentsListInput();
